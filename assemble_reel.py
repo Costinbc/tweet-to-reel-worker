aa236163ec10da07e69139a65aba50aa00898138
costinbc <costinbosoaga3@gmail.com>
2025-07-31 22:47:26 +0300

    change filters to cuda versions

--- assemble_reel.py
+++ assemble_reel.py
@@ -5,8 +5,8 @@ from PIL import Image, ImageDraw
 from crop_tweet import generate_rounded_mask
 
 LAYOUTS = {
-    "video_top"   : "[vid][img_padded]vstack_cuda[stacked];",
-    "video_bottom": "[img_padded][vid]vstack_cuda[stacked];"
+    "video_top":    "[vid][img_padded]overlay_cuda=x='(main_w-overlay_w)/2':y=main_h[stacked];",
+    "video_bottom": "[img_padded][vid]overlay_cuda=x='(main_w-overlay_w)/2':y=main_h[stacked];"
 }
 
 
@@ -31,36 +31,33 @@ def assemble(layout, background, cropped, image, video, output, mask=None):
     masked_image_path = os.path.splitext(image)[0] + "_masked.png"
     masked_image = apply_mask(image, mask, masked_image_path) if mask else image
 
-    img_branch = "[1:v]hwupload_cuda,format=yuva420p[tweet_gpu];"
-    video_branch = "[0:v]format=yuva420p,split=2[v_for_bg][v_for_main];"
+    img_branch = (
+        "[1:v]format=rgba[tweet_cpu];"
+        "[tweet_cpu]pad=1080:ih:(1080-iw)/2:0:color=0x00000000,hwupload_cuda[img_padded];"
+    )
+    video_branch = "[0:v]split=2[v_for_bg][v_for_main];"
+
 
     if background == "white":
-        bg_filter = "color=c=white:s=1080x1920,hwupload_cuda,format=yuva420p[bg_final];"
+        bg_filter = "color=c=white:s=1080x1920,format=yuv420p,hwupload_cuda[bg_final];"
     elif background == "blur":
         bg_filter = (
-            "[v_for_bg]scale_cuda=w=1080:h=1920:force_original_aspect_ratio=increase,"
-            "crop=w=1080:h=1920:x=0:y=0,gblur_cuda=sigma=15.0[bg_final];"
+            "[v_for_bg]hwupload_cuda,"
+            "scale_cuda=1080:1920:force_original_aspect_ratio=increase,"
+            "format=yuv444p,"
+            "bilateral_cuda=window_size=15:sigmaS=8:sigmaR=75,"
+            "crop=1080:1920[bg_final];"
         )
     else:
         raise ValueError("background must be 'white' or 'blur'")
 
     if cropped:
-        vid_filter = "[v_for_main]crop_cuda=w='min(iw,ih)':h='min(iw,ih)',scale_cuda=w=1080:h=1080[vid];"
+        vid_filter = (
+            "[v_for_main]crop=w='min(iw,ih)':h='min(iw,ih)',"
+            "hwupload_cuda,scale_cuda=1080:1080[vid];"
+        )
     else:
-        vid_filter = "[v_for_main]scale_cuda=w=1080:h=-2[vid];"
-
-    pad_filter = "[tweet_gpu]pad_cuda=w=1080:h=ih:x=(1080-iw)/2:y=0:color=0x00000000[img_padded];"
-
-    # if mask is not None:
-    #     img_branch += (
-    #         "[2:v]scale=iw:ih[mask];"
-    #         "[img][mask]alphamerge[rounded];"
-    #         "[rounded]pad=1080:ih:(ow-iw)/2:0:color=0x00000000[img_padded]"
-    #     )
-    # else:
-    #     img_branch += (
-    #         "[img]pad=1080:ih:(ow-iw)/2:0:color=0x00000000[img_padded]"
-    #     )
+        vid_filter = "[v_for_main]hwupload_cuda,scale_cuda=1080:-2[vid];"
 
     try:
         stack_filter = LAYOUTS[layout]
@@ -69,7 +66,7 @@ def assemble(layout, background, cropped, image, video, output, mask=None):
 
     overlay_filter = "[bg_final][stacked]overlay_cuda=x=(W-w)/2:y=((H-h)/2+70)[final]"
 
-    fc = "".join([img_branch, video_branch, bg_filter, vid_filter, pad_filter, stack_filter, overlay_filter])
+    fc = "".join([img_branch, video_branch, bg_filter, vid_filter, stack_filter, overlay_filter])
 
     cmd = [
         "/usr/local/bin/ffmpeg", "-y",

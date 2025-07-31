4438c93dfc7ed6a94fe23aebaed63da8e1fa548e
costinbc <costinbosoaga3@gmail.com>
2025-07-31 14:42:54 +0300

    add explicit ffmpeg location

--- video_dl.py
+++ video_dl.py
@@ -5,7 +5,8 @@ import yt_dlp
 def download_tweet_video(tweet_url, output_path=None):
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
-        'outtmpl': output_path
+        'outtmpl': output_path,
+        "ffmpeg_location": "/usr/bin/ffmpeg",
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
